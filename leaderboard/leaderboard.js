1f088a9b60abca6751a0381b1ba936b9e6e3ea23
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-13 17:28:29 -0700

    console.log tests

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,7 +2,9 @@
 GamersList = new Meteor.Collection('gamers');
 
 if(Meteor.isClient){
+  console.log("This code only runs in the client")
 }
 
 if(Meteor.isServer){
+  console.log("This code only runs on the server")
 }
